5e31d0932450389ab4d1d5bdf13dc1418b430358
TomC <tcuypers@gmail.com>
2022-08-11 21:20:15 +0200

    Add dis

---
+++ dist/js/about.bcbd8930.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmeso"]=self["webpackChunkmeso"]||[]).push([[443],{242:function(n,e,s){s.r(e),s.d(e,{default:function(){return l}});var u=s(252);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),r=[a];function c(n,e){return(0,u.wg)(),(0,u.iD)("div",t,r)}var o=s(744);const i={},f=(0,o.Z)(i,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.bcbd8930.js.map
